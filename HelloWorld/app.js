c34bb518f10a833461cb605335cc0c25966c4985
Smithy <shaun@softwaresmith.co.uk>
2023-01-16 22:15:01 +0000

    variable assignment

--- HelloWorld/app.js
+++ HelloWorld/app.js
@@ -1,6 +1,31 @@
-var message = 'Hello, World!';
-// create a new heading 1 element
-var heading = document.createElement('h1');
-heading.textContent = message;
-// add the heading the document
-document.body.appendChild(heading);
+function getProduct(id) {
+    return {
+        id: id,
+        name: "awesome gadget ".concat(id),
+        price: 99.5
+    };
+}
+//these errors are appearing because of the corrosponding JS file
+//  TODO: How to fix this??
+//  Only happens when the file is open??
+var product = getProduct(1);
+console.log("the product ".concat(product.name, " costs $").concat(product.price));
+//type is heading
+var heading = document.querySelector('h1');
+var counter = 1;
+counter++;
+console.log(counter);
+var names = ["shaun", "stacey", "lily", "bobby"];
+console.log(names);
+var people = [
+    {
+        name: "dave",
+        age: 100
+    }
+];
+console.log(people);
+var greeting;
+greeting = function (name) {
+    return "Hi ".concat(name);
+};
+console.log(greeting("Shaun"));
--- HelloWorld/app.ts
+++ HelloWorld/app.ts
@@ -1,6 +1,50 @@
-let message: string = 'Hello, World!';
-// create a new heading 1 element
-let heading = document.createElement('h1');
-heading.textContent = message;
-// add the heading the document
-document.body.appendChild(heading);
+interface Product{
+    id: number,
+    name: string,
+    price: number
+}
+
+function getProduct(id) : Product{
+    return { 
+        id: id,
+        name: `awesome gadget ${id}`,
+        price: 99.5
+    }
+}
+
+//these errors are appearing because of the corrosponding JS file
+//  TODO: How to fix this??
+//  Only happens when the file is open??
+const product = getProduct(1);
+console.log(`the product ${product.name} costs $${product.price}`);
+
+//type is heading
+const heading = document.querySelector('h1');
+
+let counter: number = 1;
+counter ++;
+console.log(counter);
+
+let names: string[] = ["shaun", "stacey", "lily", "bobby"];
+console.log(names);
+
+interface person {
+    name: string,
+    age: number
+}
+
+let people: person[] = [
+    {
+        name: "dave",
+        age: 100
+    }
+]
+
+console.log(people);
+
+let greeting : (name: string) => string;
+greeting = function (name: string){
+    return `Hi ${name}`;
+}
+
+console.log(greeting("Shaun"));
