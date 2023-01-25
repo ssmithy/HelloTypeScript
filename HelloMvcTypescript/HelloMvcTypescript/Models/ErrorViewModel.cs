bbb93ac434c0618f276ddddf2421d2f54ce19509
Smithy <shaun@softwaresmith.co.uk>
2023-01-25 21:30:20 +0000

    hello typescript

---
+++ HelloMvcTypescript/HelloMvcTypescript/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace HelloMvcTypescript.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
